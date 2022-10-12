3d7ac3f2cf556cd63272307ac68a8081880b86d4
maksim stryuts <maxim.stryuts@gmail.com>
2022-10-12 12:48:15 +0300

    .

---
+++ src/components/FeedbackForm/Controls.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import css from '../FeedbackForm/Controls.module.css';
+
+const Controls = ({ onPlus, onMinus, toggle, showState }) => (
+  <div className={css.controls}>
+    <button type="button" className={css.btn} onClick={onPlus}>
+      +
+    </button>
+    <button type="button" className={css.btn} onClick={toggle}>
+      {showState ? '=(' : '=)'}
+    </button>
+    <button type="button" className={css.btn} onClick={onMinus}>
+      -
+    </button>
+  </div>
+);
+
+export default Controls;
