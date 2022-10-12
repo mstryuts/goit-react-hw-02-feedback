e6eb83439907f4c6880d2a79cf49828447184080
maksim stryuts <maxim.stryuts@gmail.com>
2022-10-12 13:37:29 +0300

    .

--- src/components/FeedbackForm/FeedbackForm.jsx
+++ src/components/FeedbackForm/FeedbackForm.jsx
@@ -38,7 +38,7 @@ class Counter extends Component {
     const { value1 } = this.state;
     const { value2 } = this.state;
     const { value3 } = this.state;
-    const { visible } = this.state;
+    // const { visible } = this.state;
     return (
       <div className={css.counter}>
         <Values value1={value1} value2={value2} value3={value3} />
